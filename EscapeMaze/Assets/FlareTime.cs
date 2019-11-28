bf8702cce858efe3416094b5f29e47d11c488b3f
zmoham2 <zmoham2@uic.edu>
2019-11-27 18:22:10 -0600

    added flare - press F

---
+++ EscapeMaze/Assets/FlareTime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlareTime : MonoBehaviour
+{
+    public GameObject flare;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F) && counter == 0)
+        {
+            flare.SetActive(true);
+            Invoke("removeFlare", 10);
+        }
+
+    }
+
+    void removeFlare()
+    {
+        counter = 1;
+        Destroy(flare);
+    }
+
+}
