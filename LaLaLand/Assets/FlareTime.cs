2d191adda18cc302c63b7add95cc4f91e84bb404
MDYbarra <mybarr3@uic.edu>
2019-11-29 11:57:25 -0600

    Completed

---
+++ LaLaLand/Assets/FlareTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlareTime : MonoBehaviour
+{
+    public GameObject flare, hintUsed, flareSound;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.Joystick1Button9)) && counter == 0)
+        {
+            flare.SetActive(true);
+            hintUsed.SetActive(true);
+            flareSound.SetActive(true);
+            Invoke("removeFlare", 10);
+        }
+
+    }
+
+    void removeFlare()
+    {
+        counter = 1;
+        Destroy(flare);
+        Destroy(flareSound);
+    }
+
+}
---
+++ LaLaLand/Assets/GoalGame.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalGame : MonoBehaviour
+{
+    public GameObject SphereA, SphereB, SphereC, CorrectAudio, CorrectAudio2, IncorrectAudio, VictoryAudio, JokerWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // SphereA.SetActive(true);
+        //SphereB.SetActive(true);
+        //SphereC.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SphereC.activeSelf == false && SphereA.activeSelf == false && SphereB.activeSelf == false)
+        {
+            VictoryAudio.SetActive(true);
+            JokerWin.SetActive(true);
+            Invoke("SceneChanger", 5);
+        }
+        if (SphereC.activeSelf == false && SphereA.activeSelf == true && SphereB.activeSelf == false)
+        {
+            SphereA.SetActive(true);
+            SphereB.SetActive(true);
+            SphereC.SetActive(true);
+        }
+        if (SphereC.activeSelf == false && SphereA.activeSelf == false && SphereB.activeSelf == true)
+        {
+            CorrectAudio2.SetActive(true);
+        }
+
+        }
+
+    public void SceneChanger()
+    {
+        SceneManager.LoadScene("World_2");
+    }
+
+    public void CodeThing()
+    {
+         if (SphereA.activeSelf == false && SphereC.activeSelf == true || SphereB.activeSelf == false && SphereC.activeSelf == true)
+        {
+            SphereA.SetActive(true);
+            SphereB.SetActive(true);
+            SphereC.SetActive(true);
+            IncorrectAudio.SetActive(true);
+            CorrectAudio.SetActive(false);
+            CorrectAudio2.SetActive(false);
+        }
+     
+
+        else if (SphereC.activeSelf == false && SphereA.activeSelf == true && SphereB.activeSelf == true)
+        {
+            IncorrectAudio.SetActive(false);
+            CorrectAudio.SetActive(true);
+
+            if (SphereC.activeSelf == false && SphereA.activeSelf == false && SphereB.activeSelf == true)
+            {
+                CorrectAudio2.SetActive(true);
+
+                if (SphereC.activeSelf == false && SphereA.activeSelf == false && SphereB.activeSelf == false)
+                {
+                    VictoryAudio.SetActive(true);
+                }
+                
+            }
+            
+        }
+                
+                    
+            
+    }
+}
